d1ad73dd323e53245315ca55a39f9b64ab0efe41
Anas <anasmahmud523@gmail.com>
2022-09-03 15:59:40 +0600

    news card

--- js/index.js
+++ js/index.js
@@ -7,20 +7,32 @@ const loadAllnews = async () => {
 const setAllCategories = async () => {
     const allCategories = await loadAllnews();
     const categories = document.getElementById('categories');
-    for (const category of allCategories) {
-        // console.log(category);
+    for (const categoryId of allCategories) {
+        // console.log(categoryId);
         const li = document.createElement('li');
-        li.innerHTML = `<a onclick="loadCategory()">${category.category_name}</a>`;
+        li.innerHTML = `<a onclick="loadCategory('${categoryId.category_id}')">${categoryId.category_name}</a>`;
         categories.appendChild(li);
+        // console.log(categoryId.category_id);
     }
 }
 
-const loadCategory = async () => {
-    const url = `https://openapi.programming-hero.com/api/news/category/08`
+const loadCategory = async (categories) => {
+    // console.log(categories);
+    const url = `https://openapi.programming-hero.com/api/news/category/${categories}`
     const res = await fetch(url)
     const data = await res.json()
-    console.log(data.data[0].category_id)
-}
+    const categoriesData = data.data;
+    // console.log(categoriesData);
 
+    const categoryNews = document.getElementById('category-news');
+    for (category of categoriesData) {
+        console.log(category);
+        const div = document.createElement('div');
+        div.innerHTML = `
+        <h1>hello</h1>
+        `;
+    }
+    categoryNews.appendChild(div);
+}
 
 setAllCategories();
